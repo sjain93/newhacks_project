b4ff3ea93ddc7baca626187d3efb6c450d12f623
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-03-23 21:44:04 -0400

    commit 1

--- solardata/solardata/settings.py
+++ solardata/solardata/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '_put4sle%8wrz(#q2+sgvzjd9ft*2$^up=n(3mu%j9k9gd+8$&'
-GOOGLE_MAPS_API_KEY = 
+GOOGLE_MAPS_API_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
